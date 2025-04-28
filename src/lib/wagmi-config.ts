b3d2334c0864225c2fc7867f8a093e90f6dadf94
otis-tran <otis.tran2000@gmail.com>
2025-04-28 22:55:20 +0700

    Use Saigon testnet

--- src/lib/wagmi-config.ts
+++ src/lib/wagmi-config.ts
@@ -1,12 +1,12 @@
 // lib/wagmi-config.ts
 import { http, createConfig } from 'wagmi'
-import { mainnet, base } from 'wagmi/chains'
+import { saigon } from 'wagmi/chains'
 import { injected, walletConnect, metaMask, safe } from 'wagmi/connectors'
 
 const projectId = '570f3c3b7eff87b45e46202dfb0e21c9'
 
 export const wagmiConfig = createConfig({
-  chains: [mainnet, base],
+  chains: [saigon],
   connectors: [
     injected(),
     walletConnect({ projectId }),
@@ -14,7 +14,6 @@ export const wagmiConfig = createConfig({
     safe(),
   ],
   transports: {
-    [mainnet.id]: http(),
-    [base.id]: http(),
+    [saigon.id]: http()
   },
 })
